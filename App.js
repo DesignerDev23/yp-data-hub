12950d0044e0662c7f8df84b3c608d4d5d74d929
Musa Abdulkadir <DesignerDev23>
2024-06-21 20:23:44 +0100

    fisrt Commit

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Platform, StatusBar, ActivityIndicator } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  const handleLoadStart = () => setLoading(true);
+  const handleLoadEnd = () => setLoading(false);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
+      {loading && (
+        <ActivityIndicator
+          color="#000"
+          size="large"
+          style={styles.loading}
+        />
+      )}
+      <WebView 
+        source={{ uri: 'https://ypdatahub.com.ng/app/index' }} 
+        style={styles.webview}
+        onLoadStart={handleLoadStart}
+        onLoadEnd={handleLoadEnd}
+      />
     </View>
   );
 }
@@ -13,8 +31,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#ffffff', // Ensure the background color is white
+  },
+  webview: {
+    flex: 1,
+  },
+  loading: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
